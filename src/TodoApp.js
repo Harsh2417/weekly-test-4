78a0aab73e162468cb2736cbbe0a1cdf98f82a84
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-06-29 20:28:03 +0530

    TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import TodosLogic from './TodosLogic';
-import { TodoProvider } from './TodoContext';
+import { TodoContext } from './TodoContext';
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text }]);
+  };
+
+  const updateTodo = (id, newText) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, text: newText } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+  };
+
   return (
-    <TodoProvider>
-      <div className="todo-app">
+    <TodoContext.Provider value={{ todos, addTodo, updateTodo, deleteTodo }}>
+      <div>
         <Header />
         <TodosLogic />
       </div>
-    </TodoProvider>
+    </TodoContext.Provider>
   );
 };
 
