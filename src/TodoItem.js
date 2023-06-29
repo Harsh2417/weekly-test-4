8396be4a725dfa15e1b2ba225a81e46be6b661f6
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-06-29 20:27:36 +0530

    TodoItem.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,27 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { TodoContext } from './TodoContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const TodoItem = ({ todo }) => {
-  const { deleteTodo, updateTodo } = useContext(TodoContext);
-  const [newText, setNewText] = useState(todo.text);
   const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(todo.text);
+  const { updateTodo, deleteTodo } = useContext(TodoContext);
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
-  const handleInputChange = (e) => {
-    setNewText(e.target.value);
-  };
-
-  const handleUpdateClick = () => {
-    updateTodo({
-      ...todo,
-      text: newText
-    });
-    setIsEditing(false);
+  const handleSaveClick = () => {
+    if (editedText.trim() !== '') {
+      updateTodo(todo.id, editedText);
+      setIsEditing(false);
+    }
   };
 
   const handleDeleteClick = () => {
@@ -31,25 +24,27 @@ const TodoItem = ({ todo }) => {
   return (
     <li>
       {isEditing ? (
-        <div>
-          <input type="text" value={newText} onChange={handleInputChange} />
-          <button onClick={handleUpdateClick}>
-            <FontAwesomeIcon icon={faEdit} />
+        <>
+          <input
+            type="text"
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+          />
+          <button onClick={handleSaveClick} style={{ margin: '0.25rem' }}>
+            <span role="img" aria-label="Save">💾</span>
           </button>
-        </div>
+        </>
       ) : (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <>
           <span>{todo.text}</span>
-          <div style={{ marginLeft: '10px' }}>
-            <button onClick={handleEditClick}>
-              <FontAwesomeIcon icon={faEdit} />
-            </button>
-            <button style={{ marginLeft: '10px' }} onClick={handleDeleteClick}>
-              <FontAwesomeIcon icon={faTrash} />
-            </button>
-          </div>
-        </div>
+          <button onClick={handleEditClick} style={{ margin: '0.25rem' }}>
+            <span role="img" aria-label="Edit">✏️</span>
+          </button>
+        </>
       )}
+      <button onClick={handleDeleteClick} style={{ margin: '0.25rem' }}>
+        <span role="img" aria-label="Delete">❌</span>
+      </button>
     </li>
   );
 };
