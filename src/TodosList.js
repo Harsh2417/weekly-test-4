5a8aa1679427686f5145307b7ead2dbdff584b70
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-06-29 20:29:24 +0530

    TodosList.js

--- src/TodosList.js
+++ src/TodosList.js
@@ -1,29 +1,3 @@
-// import React, { useContext } from 'react';
-// import { TodoContext } from './TodoContext';
-// import TodoItem from './TodoItem';
-
-// const TodoList = () => {
-//   const { todos } = useContext(TodoContext);
-
-//   return (
-//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center' }}>
-//       <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '5px', width: '300px' }}>
-//         <h2>Todos</h2>
-//         <ul style={{ listStyle: 'none', padding: '0' }}>
-//           {todos.map((todo) => (
-//             <TodoItem key={todo.id} todo={todo} />
-//           ))}
-//         </ul>
-//         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10px' }}>
-//           <input type="text" placeholder="Enter a todo" style={{ width: '100%', padding: '5px' }} />
-//           <button style={{ width: '100%', marginTop: '10px' }}>Add Todo</button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TodoList;
 import React, { useContext } from 'react';
 import { TodoContext } from './TodoContext';
 import TodoItem from './TodoItem';
@@ -41,4 +15,3 @@ const TodosList = () => {
 };
 
 export default TodosList;
-
